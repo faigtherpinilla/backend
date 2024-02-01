23be31fe7d32dfb3974bca6657f2bf52fa79e19c
yeissonpigo <yeissonpigo@unisabana.edu.co>
2024-02-01 15:20:47 -0500

    aregl del codigo

--- src/controllers/inventario.js
+++ src/controllers/inventario.js
@@ -2,8 +2,8 @@ const misProductos = require ("../routes/products.router");
 
 const getProductosId = async (Id) => {
     try {
-        const producto = await misProductos.findByPk(Id);
-        return producto;
+        const producto = await misProductos.find(item => item.Id === Id);
+       return producto;
     } catch (error) {
         console.error('Error al obtener el producto por ID:', error);
         throw error;
