9fc4ad4f7d4d39f3f89a5c0482d2d57adb024ea6
G10AMAYA <giovannygonzalezamaya@gmail.com>
2024-02-19 17:37:15 -0500

    new_endpoint

--- src/index.js
+++ src/index.js
@@ -9,6 +9,19 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
+//pp.get('/saludogio', (req, res) => {
+//   const nombre= req.body.nombre
+//   res.send("hola "+ nombre)
+//})
+
+app.get("/saludo/:nombre", (req , res) =>{
+    const nombre = req.params.nombre
+    res.send("su nombre es: " + nombre)
+
+})
+
+
+
 app.listen(port, () => {
     console.log("My port: " + port)
 })
